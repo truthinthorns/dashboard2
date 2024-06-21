dac26edf5b77f84709b3b3f0aecc656f1242c525
Justin <73923621+truthinthorns@users.noreply.github.com>
2024-06-21 15:58:37 -0500

    Add Geolocation prompt

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import NavigationView from './views/NavigationView.vue'
-import { useGeolocation } from '@vueuse/core';
+import NavigationView from './views/NavigationView.vue';
 import { useWeatherStore } from './store/weather';
-// import { onBeforeMount } from 'vue';
 
-// const weatherStore = useWeatherStore();
-// const {coords, locatedAt, error, resume, pause} = useGeolocation();
 
-// useGeo
-// if (coords.value.latitude != Infinity)
-//   console.log(`coords: ${coords}`);
+const weatherStore = useWeatherStore();
+const locationAllowed = (position: any) => {
+  weatherStore.setCoords(position.coords.latitude + ","+position.coords.longitude);
+}
+navigator.geolocation.getCurrentPosition(locationAllowed);
+
 
 </script>
 
 <template>
   <div class="h-100 w-100 background">
     <NavigationView />
-    <RouterView />
+    <RouterView v-if="weatherStore.coords" />
   </div>
 </template>
 
