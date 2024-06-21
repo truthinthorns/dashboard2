dd1aa4d5bd4d0d0a185af1934687ad0ee45d5f01
Justin <73923621+truthinthorns@users.noreply.github.com>
2024-06-21 15:59:04 -0500

    Add method to set coords

--- src/store/weather.ts
+++ src/store/weather.ts
@@ -6,20 +6,23 @@ export const useWeatherStore = defineStore('weather', {
     state: () => ({
         weeklyForecast: [] as weeklyForecast[],
         hourlyForecast: [] as hourlyForecast[],
-        coords: String
+        coords: ""
     }),
     actions: {
         async getWeekly() {
             this.weeklyForecast = []
-            const result = await fetch('http://127.0.0.1:8000/weekly');
+            const result = await fetch(`${import.meta.env.VUE_APP_ROOT_API}/weekly`);
             const forecast = await result.json();
             this.weeklyForecast = forecast;
         },
         async getHourly() {
             this.hourlyForecast = []
-            const result = await fetch('http://127.0.0.1:8000/hourly');
+            const result = await fetch(`${import.meta.env.VUE_APP_ROOT_API}/hourly`);
             const forecast = await result.json();
             this.hourlyForecast = forecast;
+        },
+        setCoords(newCoords: string) {
+            this.coords = newCoords;
         }
     },
 })
